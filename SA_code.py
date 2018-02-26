e756fc9c5ea79285bdf4af7c2661428c65bad635
Caetan <caetantojeiro95@gmail.com>
2018-02-26 12:25:24 +0100

    licencia

--- SA_code.py
+++ SA_code.py
@@ -1,4 +1,4 @@
-/**********************************************************************/
+"""**********************************************************************/
 
 Copyright (C) 2016  Caetán Tojeiro Carpente
 
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/agpl-3.0.html>
 
-/**********************************************************************/
+/**********************************************************************"""
 
 import random, numpy, math
 import matplotlib.pyplot as plt
